9d8218e6ff865ffacd4752ee648c5c4f597e5f30
Vatsal Shukla <vatsalshukla@Vatsals-MacBook-Pro.local>
2015-10-31 20:54:36 -0400

    Add my contact info

--- app/views.py
+++ app/views.py
@@ -8,11 +8,11 @@ from django.template import RequestContext
 from datetime import datetime
 
 YOUR_INFO = {
-    'name' : 'Your name',
-    'bio' : 'What\'s your deal? What do you do?',
+    'name' : 'Notyfy',
+    'bio' : 'Community Inputed Alerts.',
     'email' : '', # Leave blank if you'd prefer not to share your email with other conference attendees
-    'twitter_username' : 'tweettweet', # No @ symbol, just the handle.
-    'github_username' : "fetchpush", 
+    'twitter_username' : 'Notyfy', # No @ symbol, just the handle.
+    'github_username' : "Notyfy", 
     'headshot_url' : '', # Link to your GitHub, Twitter, or Gravatar profile image.
 }
     
